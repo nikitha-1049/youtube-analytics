7caa1b3d350313953fe28e3ee684e126d479bd41
Nikitha Vemulamada <vemulamadanikitha6@gmail.com>
2026-03-12 19:48:00 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ SECRET_KEY = "tubebuddy_secret"
 MYSQL_CONFIG = {
     "host": "localhost",
     "user": "root",
-    "password": "Nikki66$$",
+    "password": "YOURPASSWORD",
     "db": "TubeBuddy"
 }
 
@@ -16,3 +16,4 @@ YOUTUBE_API_KEY = os.getenv("YOUTUBE_API_KEY")
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 
 FIXED_CHANNEL_ID = "UCq4145N44S36U991mD3h67Q"
+
